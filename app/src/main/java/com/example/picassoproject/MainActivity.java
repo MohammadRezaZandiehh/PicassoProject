7ab014554941091517ae13b81f021447bb6270ab
mohammad <m.zandieh1999@gmail.com>
2021-05-05 16:15:39 +0430

    add url

--- app/src/main/java/com/example/picassoproject/MainActivity.java
+++ app/src/main/java/com/example/picassoproject/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView imageView = findViewById(R.id.iv_main);
-        Picasso.get().load(R.drawable.wallpaper).into(imageView);
+        Picasso.get().load("https://www.google.com/search?q=wallpaper&sxsrf=ALeKk02Le5O_mHbOrGLZW0uMWWXqk8Ib5Q:1620214775762&tbm=isch&source=iu&ictx=1&fir=OHJowjYb9muRwM%252Cz_-Bdpg4srRlBM%252C_&vet=1&usg=AI4_-kQHt4ArweTiNiikHtX7hC4nMS19wQ&sa=X&ved=2ahUKEwjV1ayxurLwAhVSJBoKHWplAugQ9QF6BAgGEAE#imgrc=OHJowjYb9muRwM")
+                .into(imageView);
     }
 }
